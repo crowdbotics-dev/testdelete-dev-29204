95f89cfd06625c97b8b7fa41e95f804429338861
Crowdbotics <engineering@crowdbotics.com>
2022-08-16 09:21:35 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import TTtest
+
+admin.site.register(TTtest)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class TTtest(models.Model):
+    "Generated Model"
+    ttest = models.BigIntegerField()
