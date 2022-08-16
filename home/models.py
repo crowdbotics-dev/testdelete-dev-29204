e9460de0455eda5e79e457a580e8a91a9bd6cc69
Crowdbotics <engineering@crowdbotics.com>
2022-08-16 09:29:52 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class TTtest(models.Model):
     "Generated Model"
     ttest = models.BigIntegerField()
+    tttt = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
